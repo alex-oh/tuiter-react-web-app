0c47d2a69e70121e1400baca5ce5570d0e6cdfa4
alex-oh <oh.alexj@gmail.com>
2023-07-29 12:03:15 -0400

    lab a4 commit

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,17 +1,17 @@
-import Lab3 from "./a3";
 import Nav from "../nav";
 import { Routes, Route } from "react-router";
 import { Link } from "react-router-dom";
+import Assignment4 from "./a4";
+import Assignment3 from "./a3";
 
 function Labs() {
     return (
         <div>
             <Nav />
-            <h1>Labs</h1>
-            <Link to="lab3">Lab 3</Link>
             <Routes>
-                <Route path="/" element={<Lab3 />} />
-                <Route path="/lab3/*" element={<Lab3 />} />
+                <Route path="/" element={<Assignment3 />} />
+                <Route path="a3" element={<Assignment3 />} />
+                <Route path="a4" element={<Assignment4 />} />
             </Routes>
         </div>
     );
